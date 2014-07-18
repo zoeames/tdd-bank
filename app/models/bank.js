9ef2c93914de92abb44932bb84edce77bcfe025b
Zoe Ames <zoeames@gmail.com>
2014-07-18 12:35:01 -0500

    bank added

---
+++ app/models/bank.js
@@ -0,0 +1,11 @@
+// model file
+
+'use strict';
+
+function Bank (name){
+  this.name = name;
+  this.accounts = [];
+
+}
+
+module.exports = Bank;
---
+++ test/unit/bank.js
@@ -0,0 +1,26 @@
+// test file
+
+/*jshint expr:true*/
+/*global describe, it*/
+ 'use strict';
+
+ var expect = require('chai').expect;
+ var Bank = require('../../app/models/bank.js');
+
+ describe('Bank', function(){
+  describe ('constructor',function(){
+  it ('should create a new Bank', function(){
+    var bank = new Bank();
+    expect(bank).to.be.ok;
+    expect(bank).to.be.instanceof(Bank);
+  });
+  it('should create a new Bank with arguments',function(){
+    var bank = new Bank('Chase');
+    expect(bank.name).to.equal ('Chase');
+    expect (bank.accounts).to.be.a('array');
+    expect(bank.accounts).to.have.length(0); 
+  });
+  });
+ });
+
+ 
